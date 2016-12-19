45e878c7a41d5c862ee0921a66a8884c23f6632c
Trickstercard101 <boearles@gmail.com>
2016-12-19 16:08:26 -0600

    Create christmastree.py

---
+++ christmastree.py
@@ -0,0 +1,15 @@
+import turtle
+
+t = turtle.Turtle()
+
+t.color('brown')
+
+for i in range(13):
+	t.forward(30)
+    t.left(90)
+    t.forward(1)
+    t.left(90)
+    t.forward(30)
+    t.right(90)
+    t.forward(1)
+    t.right(90)
